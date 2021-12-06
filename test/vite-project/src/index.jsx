ac1ca2b8d4e8711cea04d771f9e99961443c1735
freebebe <gogofreebebe@tutamail.com>
2021-12-06 15:40:50 +1100

    testdemo

---
+++ test/vite-project/src/index.jsx
@@ -0,0 +1,13 @@
+import Head from 'next/head'
+
+const App = () => {
+  return (
+    <div className="">
+      <Head>
+        <title>test</title>
+      </Head>
+    </div>
+  )
+}
+
+export default App
